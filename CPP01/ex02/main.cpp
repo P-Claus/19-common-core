896025f75c85876018433c85ca1a4cd5388197dd
Pieter Claus <pieter.claus1@gmail.com>
2024-09-17 21:37:01 +0200

    finish ex02

---
+++ CPP01/ex02/main.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <iostream>
+
+int	main(void)
+{
+	std::string string = "HI THIS IS BRAIN";
+
+	std::string* stringPTR = &string;
+	std::string& stringREF = string;
+
+	std::cout << "Address of the string: " << &string << std::endl;
+	std::cout << "Address of stringPTR: " << &stringPTR << std::endl;
+	std::cout << "Address of stringREF: " << &stringREF << std::endl;
+
+	std::cout << "Value of the string: " << string << std::endl;
+	std::cout << "Value of stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value of stringREF: " << stringREF << std::endl;
+}
