abf5ac8ac968a75f7ebf0e00968f444366e9098b
Pieter Claus <pieter.claus1@gmail.com>
2024-09-24 14:27:41 +0200

    finish ex00

--- CPP03/ex00/src/ClapTrap.cpp
+++ CPP03/ex00/src/ClapTrap.cpp
@@ -14,8 +14,8 @@ ClapTrap::ClapTrap(const std::string string)
 {
     std::cout << "String constructor called" << std::endl;
 	hitPoints = 10;
-	energyPoints = 10;
-	attackDamage = 0;
+	energyPoints = 6;
+	attackDamage = 3;
 	name = string;
     return ;
 }
@@ -45,5 +45,37 @@ ClapTrap::~ClapTrap(void)
 
 void ClapTrap::attack(const std::string& target)
 {
-	std::cout << "ClapTrap " << name << " attacks " << target << ", causing " << attackDamage << " points of damage!" << std::endl; 
+	if (energyPoints > 0 && hitPoints > 0)
+	{
+		std::cout << "ClapTrap " << name << " attacks " << target << ", causing " << attackDamage << " points of damage!"; 
+		energyPoints--;
+		std::cout << " || EP left: " << energyPoints << std::endl;
+	}
+	else {
+		std::cout << "ClapTrap " << name << " has no energy or hitpoints left!" << std::endl;;
+	}
+
+}
+
+void	ClapTrap::takeDamage(unsigned int amount)
+{
+		std::cout << "ClapTrap " << name << " is hit, losing " << amount << " hitpoints!";
+		
+		hitPoints -= amount;
+		std::cout << " || HP left: " << hitPoints << std::endl;
+}
+
+void	ClapTrap::beRepaired(unsigned int amount)
+{
+	if (energyPoints > 0)
+	{
+		std::cout << "ClapTrap " << name << " repairs itself and gains " << amount << " hitpoint(s).";
+		hitPoints += amount;
+		energyPoints--;
+		std::cout << " || HP left: " << hitPoints << " || EP left: " << energyPoints<< std::endl;
+	}
+	else {
+		std::cout << "ClapTrap " << name << " has no energy or hitpoints left!" << std::endl;;
+	}
+
 }
--- CPP03/ex00/src/main.cpp
+++ CPP03/ex00/src/main.cpp
@@ -2,6 +2,15 @@
 
 int	main(void)
 {
-	ClapTrap	claptrap;
 	ClapTrap	clapclap("CLAP");
+	clapclap.attack("clap");
+	clapclap.attack("clap");
+	clapclap.attack("clap");
+	clapclap.attack("clap");
+
+	clapclap.takeDamage(3);
+	clapclap.takeDamage(6);
+
+	clapclap.beRepaired(2);
+	clapclap.beRepaired(4);
 }
