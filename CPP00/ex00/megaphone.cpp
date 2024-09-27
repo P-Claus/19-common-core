b23eacf0503c78d80f27315988d0fc1f228913e5
Pieter Claus <pieter.claus1@gmail.com>
2024-09-27 16:13:37 +0200

    final checks

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -6,18 +6,19 @@
 /*   By: pclaus <pclaus@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 19:48:56 by pclaus            #+#    #+#             */
-/*   Updated: 2024/09/27 13:32:37 by pclaus           ###   ########.fr       */
+/*   Updated: 2024/09/27 15:44:02 by pclaus           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string>
 
 int	main(int argc, char ** argv)
 {
-	int				iter (0);
-	int				counter (1);
+	int		iter (0);
+	int		counter (1);
 	std::string 	wo_args = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	std::string		curr_arg;
+	std::string	curr_arg;
 
 	if (argc == 1)
 		std::cout << wo_args << std::endl;
