df5e8ae1a89d323d274505738dd944aef155cc53
Pieter Claus <pieter.claus1@gmail.com>
2024-09-27 18:26:55 +0200

    added more print statements to see the differences in address

--- CPP01/ex02/main.cpp
+++ CPP01/ex02/main.cpp
@@ -9,9 +9,13 @@ int	main(void)
 	std::string& stringREF = string;
 
 	std::cout << "Address of the string: " << &string << std::endl;
-	std::cout << "Address of stringPTR: " << stringPTR << std::endl;
+	std::cout << "Address of stringPTR: " << &stringPTR << std::endl;
 	std::cout << "Address of stringREF: " << &stringREF << std::endl;
 
+	std::cout << "Address of the string: " << &string << std::endl;
+	std::cout << "Address of the string through stringPTR: " << stringPTR << std::endl;
+	std::cout << "Address of the string through stringREF: " << &stringREF << std::endl;
+	
 	std::cout << "Value of the string: " << string << std::endl;
 	std::cout << "Value of stringPTR: " << *stringPTR << std::endl;
 	std::cout << "Value of stringREF: " << stringREF << std::endl;
