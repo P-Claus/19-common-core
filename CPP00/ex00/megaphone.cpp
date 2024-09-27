22decf4c373c41b3c7f489d613c4f3af07b40cd2
Pieter Claus <pieter.claus1@gmail.com>
2024-09-27 08:55:03 +0200

    finish ex00

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -1,31 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ex00.cpp                                           :+:      :+:    :+:   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pclaus <pclaus@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 19:48:56 by pclaus            #+#    #+#             */
-/*   Updated: 2024/06/11 21:55:40 by pclaus           ###   ########.fr       */
+/*   Updated: 2024/09/27 08:52:17 by pieter           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <ctype.h>
 
 int	main(int argc, char ** argv)
 {
-	int	iter (0);
-	int	counter (1);
+	int				iter (0);
+	int				counter (1);
+	std::string 	wo_args = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	std::string		curr_arg;
 
 	if (argc == 1)
-		std::cout << ("* LOUD AND UNBEARABLE FEEDBACK NOISE *\n");
+		std::cout << wo_args << std::endl;
 	else
 	{
 		while (counter < argc)
 		{
-			while (argv[counter][iter] != '\0')
-				std::cout << (char)toupper(argv[counter][iter++]);
+			curr_arg = argv[counter];
+			while (curr_arg[iter] != '\0')
+				std::cout << (char)toupper(curr_arg[iter++]);
 			std::cout << " ";
 			iter = 0;
 			counter++;
