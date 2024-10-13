ef7bb9e3448f2ac0167f01b2a565e625773673df
Pieter Claus <pieter.claus1@gmail.com>
2024-10-13 18:01:14 +0200

    add explicit instatiation when the template functions are called

--- CPP07/ex00/src/main.cpp
+++ CPP07/ex00/src/main.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pieter <pclaus@student.s19.be>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/13 18:00:10 by pieter            #+#    #+#             */
+/*   Updated: 2024/10/13 18:00:28 by pieter           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/whatever.hpp"
 
 int	main(void)
@@ -6,10 +18,10 @@ int	main(void)
 	int	b = 3;
 
 	std::cout << "Before swap: a = " << a << ", b = " << b << std::endl;
-	::swap(a, b);
+	::swap<int>(a, b);
 	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::cout << "min( a, b ) = " << ::min<int>( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max<int>( a, b ) << std::endl;
 
 	std::cout << "-------------------------" << std::endl;
 
@@ -17,10 +29,10 @@ int	main(void)
 	std::string d = "robot";
 
 	std::cout << "Before c = " << c << ", d = " << d << std::endl;
-	::swap(c, d);
+	::swap<std::string>(c, d);
 	std::cout << "c = " << c << ", d = " << d << std::endl;
-	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	std::cout << "min( c, d ) = " << ::min<std::string>( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max<std::string>( c, d ) << std::endl;
 
 	std::cout << "-------------------------" << std::endl;
 
@@ -28,10 +40,10 @@ int	main(void)
 	char	f = 'f';
 
 	std::cout << "Before e = " << e << ", f = " << f << std::endl;
-	::swap(e, f);
+	::swap<char>(e, f);
 	std::cout << "e = " << e << ", f = " << f << std::endl;
-	std::cout << "min( e, f ) = " << ::min( e, f ) << std::endl;
-	std::cout << "max( e, f ) = " << ::max( e, f ) << std::endl;
+	std::cout << "min( e, f ) = " << ::min<char>( e, f ) << std::endl;
+	std::cout << "max( e, f ) = " << ::max<char>( e, f ) << std::endl;
 
 	std::cout << "-------------------------" << std::endl;
 
@@ -39,8 +51,8 @@ int	main(void)
 	float	h = 42.42f;
 
 	std::cout << "Before g = " << g << ", h = " << h << std::endl;
-	::swap(g, h);
+	::swap<float>(g, h);
 	std::cout << "g = " << g << ", h = " << h << std::endl;
-	std::cout << "min( g, h ) = " << ::min( g, h ) << std::endl;
-	std::cout << "max( g, h ) = " << ::max( g, h ) << std::endl;
+	std::cout << "min( g, h ) = " << ::min<float>( g, h ) << std::endl;
+	std::cout << "max( g, h ) = " << ::max<float>( g, h ) << std::endl;
 }
