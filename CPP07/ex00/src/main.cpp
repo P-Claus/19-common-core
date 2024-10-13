845666b40fec69de3a5517e384452ce3f0b54cd1
Pieter Claus <pieter.claus1@gmail.com>
2024-10-13 15:25:36 +0200

    finish ex00 and it works with simple type and with the file provided during the eval

---
+++ CPP07/ex00/src/main.cpp
@@ -0,0 +1,46 @@
+#include "../includes/whatever.hpp"
+
+int	main(void)
+{
+	int a = 2;
+	int	b = 3;
+
+	std::cout << "Before swap: a = " << a << ", b = " << b << std::endl;
+	::swap(a, b);
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+	std::cout << "-------------------------" << std::endl;
+
+	std::string c = "mister";
+	std::string d = "robot";
+
+	std::cout << "Before c = " << c << ", d = " << d << std::endl;
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+	std::cout << "-------------------------" << std::endl;
+
+	char	e = 'e';
+	char	f = 'f';
+
+	std::cout << "Before e = " << e << ", f = " << f << std::endl;
+	::swap(e, f);
+	std::cout << "e = " << e << ", f = " << f << std::endl;
+	std::cout << "min( e, f ) = " << ::min( e, f ) << std::endl;
+	std::cout << "max( e, f ) = " << ::max( e, f ) << std::endl;
+
+	std::cout << "-------------------------" << std::endl;
+
+	float	g = 19.19f;
+	float	h = 42.42f;
+
+	std::cout << "Before g = " << g << ", h = " << h << std::endl;
+	::swap(g, h);
+	std::cout << "g = " << g << ", h = " << h << std::endl;
+	std::cout << "min( g, h ) = " << ::min( g, h ) << std::endl;
+	std::cout << "max( g, h ) = " << ::max( g, h ) << std::endl;
+}
