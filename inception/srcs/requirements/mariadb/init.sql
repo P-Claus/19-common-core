cb99ee8b9926271b65620387d39055cf5a20ff25
Pieter Claus <pieter.claus1@gmail.com>
2024-11-13 14:20:43 +0100

    changed some things in the sql init script to check if the db and user already exist

--- inception/srcs/requirements/mariadb/init.sql
+++ inception/srcs/requirements/mariadb/init.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE mariadb_db;
-CREATE USER 'pclaus'@'%' IDENTIFIED BY 'pclaus';
+CREATE DATABASE IF NOT EXISTS mariadb_db;
+CREATE USER IF NOT EXISTS'pclaus'@'%' IDENTIFIED BY 'pclaus';
 GRANT ALL PRIVILEGES ON mariadb_db.* TO 'pclaus'@'%';
 FLUSH PRIVILEGES;
