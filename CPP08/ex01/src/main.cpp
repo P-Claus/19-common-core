eaf83ab349e170aaf9a209d3f1febee8494d5364
Pieter Claus <pieter.claus1@gmail.com>
2024-12-06 09:13:31 +0100

    added more tests to the main to see if the addMany() function works

--- CPP08/ex01/src/main.cpp
+++ CPP08/ex01/src/main.cpp
@@ -13,9 +13,9 @@ int generateRandomNumber(int min, int max)
 int	main(void)
 {
 	Span sp = Span(5);
+	Span largeSpan = Span(10);
 	
 	try {
-		//sp.addMany();
 
 		sp.addNumber(6);
 		sp.addNumber(3);
@@ -27,6 +27,17 @@ int	main(void)
 		
 		std::cout << "Shortest span: " << sp.shortestSpan() << std::endl;
 		std::cout << "Longest span: " << sp.longestSpan() << std::endl;
+
+		std::cout << "------- LARGE --------" << std::endl;
+
+		largeSpan.addMany();
+		
+		largeSpan.printContent();
+
+		std::cout << "Shortest span: " << largeSpan.shortestSpan() << std::endl;
+		std::cout << "Longest span: " << largeSpan.longestSpan() << std::endl;
+
+
 
 	} catch (const std::exception& e) {
 		std::cout << e.what() << std::endl;
