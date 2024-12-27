d2f4229cf6b2917e68b735201b087c2233450612
P-Claus <pieter.claus1@gmail.com>
2024-12-27 14:54:18 +0100

    changed some formatting in the main file

--- CPP09/ex02/src/main.cpp
+++ CPP09/ex02/src/main.cpp
@@ -9,22 +9,19 @@ int	main(int argc, char **argv)
 	std::deque<int>		numbersDeque;
 
 	struct timespec start, end;
+
 	clock_gettime(CLOCK_REALTIME, &start);
-	
 	putDataInContainer(argv, numbersVector);
 	printVectorBefore(numbersVector);
 	handleVector(numbersVector);
-
 	clock_gettime(CLOCK_REALTIME, &end);
 	double duration = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e6;
 	std::cout << "Sorting a vector of [" << numbersVector.size() << "] elements took " << duration << " microseconds." << std::endl;
 	std::cout << std::endl;
 
 	clock_gettime(CLOCK_REALTIME, &start);
-
 	putDataInContainer(argv, numbersDeque);
 	handleDeque(numbersDeque);
-
 	clock_gettime(CLOCK_REALTIME, &end);
 	duration = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e6;
 	std::cout << "Sorting a deque of [" << numbersDeque.size() << "] elements took " << duration << " microseconds." << std::endl;
